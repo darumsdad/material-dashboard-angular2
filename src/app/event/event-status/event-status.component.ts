2656467f41366215c87f2e7b4713ebdf8f951bc1
darumsdad <darumsdad@gmail.com>
2022-11-22 14:11:40 -0500

    more

--- src/app/event/event-status/event-status.component.ts
+++ src/app/event/event-status/event-status.component.ts
@@ -1,6 +1,25 @@
 import { Component,  OnInit, Optional } from '@angular/core';
 import { ControlContainer, FormControl, FormControlDirective, FormGroup, FormGroupDirective } from '@angular/forms';
 
+export var STATUS_MAP: any[] = [
+  { id: '0', name: '(Default) Hold' },
+  { id: '1', name: 'Lead' },
+  { id: '2', name: 'Proposal Sent'}, // (2 days after Lead )' },
+  { id: '4', name: 'Proposal Accepted' },
+  { id: '5', name: 'Pre-Contract Questionnaire Sent' }, // (2 days after Proposal Accepted)' },
+  { id: '6', name: 'Pre-Contract Questionnaire Completed' },
+  { id: '7', name: 'Contract Sent' }, // (2 days after Pre-Contract Questionnaire Completed)' },
+  { id: '8', name: 'Contract Received'}, // - Deposit Pending, Contract and Deposit Received' },
+  { id: '9', name: 'Pre-Wedding Meeting Scheduled'}, // (2-3 weeks prior to Wedding Date)' },
+  { id: '10', name: 'Pre-Wedding Questionnaire Sent'} , // (3 days prior to meeting)' },
+  { id: '11', name: 'Pre-Wedding Meeting Complete'}, // Wedding Paid in Full (no later than Wedding Date)' },
+  { id: '12', name: 'Highlight Song Selection Pending' },
+  { id: '13', name: 'Highlight Song Selected' },
+  { id: '14', name: 'Highlight Video Production Pending'}, // (3-4 weeks after Song Selected)' },
+  { id: '15', name: 'Highlight Video Production Completed/Pending Approval' },
+  { id: '16', name: 'Highlight Video Approved' }
+]
+
 @Component({
   selector: 'app-event-status',
   templateUrl: './event-status.component.html',
@@ -14,27 +33,11 @@ export class EventStatusComponent implements OnInit {
     ) { }
 
   form: FormGroup
+  statuses: any  = STATUS_MAP;
 
   eventTypes: any[] = ['Wedding', 'Mitzvah', 'Other']
 
-  statuses: any[] = [
-    { id: '0', name: '(Default) Hold' },
-    { id: '1', name: 'Lead' },
-    { id: '2', name: 'Proposal Sent (2 days after Lead )' },
-    { id: '4', name: 'Proposal Accepted' },
-    { id: '5', name: 'Pre-Contract Questionnaire Sent (2 days after Proposal Accepted)' },
-    { id: '6', name: 'Pre-Contract Questionnaire Completed' },
-    { id: '7', name: 'Contract Sent (2 days after Pre-Contract Questionnaire Completed)' },
-    { id: '8', name: 'Contract Received - Deposit Pending, Contract and Deposit Received' },
-    { id: '9', name: 'Pre-Wedding Meeting Scheduled (2-3 weeks prior to Wedding Date)' },
-    { id: '10', name: 'Pre-Wedding Questionnaire Sent (3 days prior to meeting)' },
-    { id: '11', name: 'Pre-Wedding Meeting Complete, Wedding Paid in Full (no later than Wedding Date)' },
-    { id: '12', name: 'Highlight Song Selection Pending' },
-    { id: '13', name: 'Highlight Song Selected' },
-    { id: '14', name: 'Highlight Video Production Pending (3-4 weeks after Song Selected)' },
-    { id: '15', name: 'Highlight Video Production Completed/Pending Approval' },
-    { id: '16', name: 'Highlight Video Approved' }
-  ]
+  
 
   ngOnInit(): void {
     console.log("set form")
